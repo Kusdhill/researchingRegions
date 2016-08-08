e7dca552776ac326d0c18cdf44573c2253e210a4
Kunal Dhillon <kusdhill@ucsc.edu>
2016-08-08 14:34:22 -0700

    working biosample field on json return

--- researchingRegions.py
+++ researchingRegions.py
@@ -14,8 +14,8 @@ import collections
 dataset = c.search_datasets().next()
 
 for functionalVariantSet in c.search_variant_sets(dataset.id):
-    if functionalVariantSet.name == "functional-annotation":
-        functionalAnnotation = functionalVariantSet
+	if functionalVariantSet.name == "functional-annotation":
+		functionalAnnotation = functionalVariantSet
 
 functionalAnnotationSet = c.search_variant_annotation_sets(variant_set_id=functionalAnnotation.id).next()
 
@@ -49,32 +49,34 @@ chrome='17'
 
 searchedVarAnns = c.search_variant_annotations(variant_annotation_set_id=functionalAnnotationSet.id, start=geneList[0]['start'], end=geneList[0]['end'], reference_name=geneList[0]['chrome'], effects=[{'id': geneList[0]['term']}])
 
-    
+	
 
 idList = []
 termList = []
 for annotation in searchedVarAnns:
-    for i in range(0,len(annotation.transcript_effects)):
-        for j in range(0,len(annotation.transcript_effects[i].effects)):
-            termList.append(annotation.transcript_effects[i].effects[j].term)
-            #print(annotation)
-            idList.append(annotation.variant_id)
+	for i in range(0,len(annotation.transcript_effects)):
+		for j in range(0,len(annotation.transcript_effects[i].effects)):
+			termList.append(annotation.transcript_effects[i].effects[j].term)
+			#print(annotation)
+			idList.append(annotation.variant_id)
 
 #print(idList)
 print(termList)
 
 functionalList = []
 for i in range(0,len(idList)):
-    gotten = c.get_variant(idList[i])
-    functionalDict = collections.OrderedDict()
-    
-    functionalDict['start'] = gotten.start
-    functionalDict['end'] = gotten.end
-    functionalDict['term'] = termList[i]
-    functionalDict['chrome'] = gotten.reference_name
-    
-    functionalList.append(functionalDict)
-    
+	gotten = c.get_variant(idList[i])
+	functionalDict = collections.OrderedDict()
+	
+	functionalDict['start'] = gotten.start
+	functionalDict['end'] = gotten.end
+	functionalDict['term'] = termList[i]
+	functionalDict['chrome'] = gotten.reference_name
+	
+	functionalList.append(functionalDict)
+	
+
+
 #print(functionalDict)
 #print(functionalList[0]['start'])
 
@@ -82,28 +84,28 @@ for i in range(0,len(idList)):
 # In[5]:
 
 for phaseVariantSet in c.search_variant_sets(dataset.id):
-    if phaseVariantSet.name == "phase3-release":
-        phaseAnnotation = phaseVariantSet
+	if phaseVariantSet.name == "phase3-release":
+		phaseAnnotation = phaseVariantSet
 
 allCallSets = list(c.search_call_sets(phaseAnnotation.id))
 
 callSetIds = []
 for callSet in allCallSets:
-    callSetIds.append(str(callSet.id))
+	callSetIds.append(str(callSet.id))
 
 phaseAnnotationSetList = []
 for i in range(0,len(functionalList)):
-    searchResults = c.search_variants(phaseAnnotation.id, start=functionalList[i]['start'], end=functionalList[i]['end'], reference_name=functionalList[i]['chrome'], call_set_ids=callSetIds)
-    for results in searchResults:
-        phaseAnnotationSetList.append(results)
-        
+	searchResults = c.search_variants(phaseAnnotation.id, start=functionalList[i]['start'], end=functionalList[i]['end'], reference_name=functionalList[i]['chrome'], call_set_ids=callSetIds)
+	for results in searchResults:
+		phaseAnnotationSetList.append(results)
+		
 #print(len(phaseAnnotationSetList))
 
 
 for i in range(0,len(phaseAnnotationSetList)):
-    for j in range(0,len(phaseAnnotationSetList[i].calls)):
-        if phaseAnnotationSetList[i].calls[j].genotype[0]==1 or phaseAnnotationSetList[i].calls[j].genotype[1]==1:
-            #print(phaseAnnotationSetList[i].calls[j])
-        
-            print unicode(phaseAnnotationSetList[i].calls[j].call_set_name+" has "+str(termList[i])+" in gene "+geneList[0]['name']+" at position "+str(functionalList[i]['start'])+" to "+str(functionalList[i]['end']))
+	for j in range(0,len(phaseAnnotationSetList[i].calls)):
+		if phaseAnnotationSetList[i].calls[j].genotype[0]==1 or phaseAnnotationSetList[i].calls[j].genotype[1]==1:
+			#print(phaseAnnotationSetList[i].calls[j])
+		
+			print unicode(phaseAnnotationSetList[i].calls[j].call_set_name+" has "+str(termList[i])+" in gene "+geneList[0]['name']+" at position "+str(functionalList[i]['start'])+" to "+str(functionalList[i]['end']))
 
