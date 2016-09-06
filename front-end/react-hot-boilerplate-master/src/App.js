52aea7b41fd74c52a7653d04c6840fdee0d57a37
Kunal Dhillon <kusdhill@ucsc.edu>
2016-09-06 13:53:01 -0700

    working HTTP request in react

--- front-end/react-hot-boilerplate-master/src/App.js
+++ front-end/react-hot-boilerplate-master/src/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import Request from 'react-http-request';
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', "http://ipinfo.io/json", true);
+var lol = xhr.send();
 
 export default class App extends Component {
-  render() {
-    return (
-      <h1>Hello, world.</h1>
-    );
-  }
+	render() {
+		return (
+			<Request
+        		url='https://api.github.com/users/kusdhill'
+        		method='get'
+        		accept='application/json'
+        		verbose={true}
+      		>
+        		{
+          			({error, result, loading}) => {
+            			if (loading) {
+            				return <div>loading...</div>;
+            			} else {
+              				return <div>{ JSON.stringify(result) }</div>;
+            			}
+          			}
+        		}
+      		</Request>
+		);
+	}
 }
+
