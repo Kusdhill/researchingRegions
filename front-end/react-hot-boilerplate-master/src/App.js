50fc86b26cd472e8fb97f47c297be81cdfdb5756
Kunal Dhillon <kusdhill@ucsc.edu>
2016-09-07 12:01:44 -0700

    working JSON return and updated requirements.txt

--- front-end/react-hot-boilerplate-master/src/App.js
+++ front-end/react-hot-boilerplate-master/src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import Request from 'react-http-request';
 
-var xhr = new XMLHttpRequest();
-xhr.open('GET', "http://ipinfo.io/json", true);
-var lol = xhr.send();
-
 export default class App extends Component {
 	render() {
 		return (
