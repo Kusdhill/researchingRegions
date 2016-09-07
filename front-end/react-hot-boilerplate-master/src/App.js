e67133e3449d71e3761fbcb698320829e9cd94a5
Kunal Dhillon <kusdhill@ucsc.edu>
2016-09-07 14:29:24 -0700

    created program skeleton, working way through request errors

--- front-end/react-hot-boilerplate-master/src/App.js
+++ front-end/react-hot-boilerplate-master/src/App.js
@@ -1,26 +1,68 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import Request from 'react-http-request';
 
-export default class App extends Component {
+// sends request to gene search service
+var request = React.createClass({
 	render() {
 		return (
 			<Request
-        		url='http://localhost:5000/gene/APP/term/SO:0001630/page/0'
-        		method='get'
-        		accept='application/json'
-        		verbose={true}
-      		>
-        		{
-          			({error, result, loading}) => {
-            			if (loading) {
-            				return <div>loading...</div>;
-            			} else {
-              				return <div>{ JSON.stringify(result) }</div>;
-            			}
-          			}
-        		}
-      		</Request>
+				url='http://localhost:5000/gene/APP/term/SO:0001630/page/0'
+				method='get'
+				accept='application/json'
+				verbose={true}
+			>
+				{
+					({error, result, loading}) => {
+						if (loading) {
+							return <div>loading...</div>;
+						} else {
+							return <div>{ JSON.stringify(result) }</div>;
+						}
+					}
+				}
+			</Request>
 		);
 	}
-}
+});
+
+/*
+
+// renders searchBox and results
+var app = React.createClass({
+	render: function(){
+		return(
+			<div>
+				<searchBox />
+				<results />
+			</div>
+		);
+	}
+});
+
+
+
+// renders resultItems
+var results = React.createClass({
+	render: function(){
+		return(
+			<ul>
+				<resultItems />
+			</ul>
+		);
+	}
+});
+
+
+
+// renders search result items
+var resultItems = React.createClass({
+	render: function(){
+		return <li>"search result item"</li>;
+	}
+});
+
+*/
+
+ReactDOM.render(<app />, document.getElementById("root"));
 
