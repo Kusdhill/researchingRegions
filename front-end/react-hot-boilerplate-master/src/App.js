728f5bbb2b427a7de303d4cd8b2e43d24dc0ea98
Kunal Dhillon <kusdhill@ucsc.edu>
2016-09-29 12:56:51 -0700

    dynamically updates table

--- front-end/react-hot-boilerplate-master/src/App.js
+++ front-end/react-hot-boilerplate-master/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Request from 'react-http-request';
 import $ from 'jquery';
+import {Table, Column, Cell} from 'fixed-data-table';
 
 // Condense all of these classes and methods into one class, look at David's slack for directions
 
@@ -9,42 +10,13 @@ import $ from 'jquery';
 export default class App extends Component{
 	render() {
 		return (
-			<div>
-				
+			<div>				
 				<Search />
 			</div>
 		)
 	}
 }
 
-
-
-//input
-
-/*
-class Input extends Component{
-	handleChange() {
-		console.log("handling change")
-		console.log(this.refs.theGene.value)
-		console.log(this.refs.theTerm.value)
-		Search.props.render.bind(
-			this.refs.theGene.value,
-			this.refs.theTerm.value
-		);
-	}
-	render() {
-		return (
-			<div>
-				<form>
-					<input type="text" placeholder="Gene" value={this.props.geneSearch} ref="theGene" />
-					<input type="text" placeholder="Term" value={this.props.termSearch} ref="theTerm" />
-				</form>
-				<br></br>
-			</div>
-		);
-	}
-}
-*/
 
 
 //search
@@ -60,7 +32,6 @@ class Search extends Component{
 		this.handleChange();
 		this.setState({clicked: true});
 	}
-	////////////////////////////////////////////////////////////
 	handleChange() {
 		console.log("handling change")
 		console.log(this.refs.theGene.value)
@@ -70,7 +41,6 @@ class Search extends Component{
 			this.refs.theTerm.value
 		);
 	}
-	////////////////////////////////////////////////////////////
 	render(userGene, userTerm) {
 		if (this.state.clicked) {
 			return <Listing userGene={this.refs.theGene.value} userTerm={this.refs.theTerm.value}/>
@@ -78,7 +48,7 @@ class Search extends Component{
 			return (
 				<div>
 					<form>
-						<input type="text" placeholder="Gene" value={this.props.geneSearch} ref="theGene" />
+						<input type="text" placeholder="Gene" value={this.props.geneSearch} ref="theGene"/>
 						<input type="text" placeholder="Term" value={this.props.termSearch} ref="theTerm" />
 					</form>
 					<br></br>
@@ -100,7 +70,7 @@ class Listing extends Component{
 		}
 	}
 	loadFromServer(pageToken=0) {
-		console.log(pageToken)
+		//console.log(pageToken)
 		let type = {'content-type': 'application/json'};
 		this.serverRequest = $.ajax({ 
 			url: "http://localhost:5000/gene/"+this.props.userGene+"/term/"+this.props.userTerm+"/page/"+pageToken, 
@@ -131,18 +101,46 @@ class Listing extends Component{
 		if (this.state.matches.length==0) {
 			return <div>loading...</div>
 		};
-		let allMatches = this.state.matches;
-		return (
-			<div>
-			{allMatches.map((matches) => {
-				return(
-					<div>
-						{matches.biosample.name}
-					</div>
 
-				)
-			})};
-			</div>
+		let allMatches = this.state.matches;
+		if (this.state.matches.length > 0){
+			{console.log("sending to Datatable")}
+			return (
+				<div>
+					<Datatable tableMatches={allMatches}/>
+				</div>
+			)
+		};
+	}
+}
+
+const TextCell = ({rowIndex, data, cola, colb, ...props}) => (
+  <Cell {...props}>
+    {data[rowIndex][cola][colb]}
+  </Cell>
+);
+
+// table
+class Datatable extends Component{
+	render() {
+		console.log("in Datatable, render")
+		console.log(this.props.tableMatches)
+		//var link = "https://www.ncbi.nlm.nih.gov/projects/SNP/snp_ref.cgi?rs=" + this.props.tableMatches.variant.names
+		//console.log(link)
+		return(
+			<Table
+				rowHeight={50}
+				headerHeight={50}
+				width={screen.width-100}
+				height={300}
+				rowsCount={this.props.tableMatches.length}>
+				<Column
+					header={<Cell>id</Cell>}
+					cell={<TextCell data={this.props.tableMatches} cola="biosample" colb="name" />}
+					columnKey="id"
+					width={200}
+				/>
+			</Table>
 		)
 	}
 }
